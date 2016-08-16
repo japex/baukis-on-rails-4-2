64ce8b15c854697b55b065a97b0772dff18616fd
Naoyuki Kumagai <kumagai@namaka>
2016-08-16 10:36:30 +0900

    Make model AppSetting singleton
    app/models/app_setting.rb
    - Make the class singleton by hiding new(), create(),
      and create!() and adding self.instance().
    - Add validations.
    - Add self.session_timeout_in_min().

--- app/models/app_setting.rb
+++ app/models/app_setting.rb
@@ -1,6 +1,25 @@
 class AppSetting < ActiveRecord::Base
+  private_class_method :new, :create, :create!
+
+  validates :application_name, presence: true, length: { maximum: 16 }
+  validates :session_timeout_in_min, presence: true, \
+                                     numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  DEFAULT_APPLICATION_NAME = 'BAUKIS'
+  DEFAULT_SESSION_TIMEOUT_IN_MIN = 60
+
+  def self.instance
+    first || create!(
+                      application_name:       DEFAULT_APPLICATION_NAME,
+                      session_timeout_in_min: DEFAULT_SESSION_TIMEOUT_IN_MIN,
+                    )
+  end
 
   def self.application_name
-    order(:updated_at).last.application_name
+    instance.application_name
+  end
+
+  def self.session_timeout_in_min
+    instance.session_timeout_in_min
   end
 end
